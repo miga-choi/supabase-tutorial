4511514e92838c08644eb24337b8684d032664a9
miga-choi <miga.choi.1004@gmail.com>
2024-06-23 00:24:46 +0900

    flutter: connect to supabase

---
+++ flutter_supabase/lib/main.dart
@@ -0,0 +1,52 @@
+import "package:flutter/material.dart";
+import "package:flutter_supabase/Config.dart";
+import "package:supabase_flutter/supabase_flutter.dart";
+
+void main() async {
+  await Supabase.initialize(
+    url: Config.supabaseUrl,
+    anonKey: Config.supabaseAnonKey,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+      ),
+    );
+  }
+}
