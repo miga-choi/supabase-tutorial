33f369c647d6bba0483451ac8b84ac49b64bdab2
miga-choi <miga.choi.1004@gmail.com>
2024-06-30 23:07:54 +0900

    flutter: try "ilike" column matches a case-insensitive pattern

--- flutter_supabase/lib/main.dart
+++ flutter_supabase/lib/main.dart
@@ -108,11 +108,17 @@ class _MyHomePageState extends State<MyHomePage> {
     print(data);
   }
 
+  void columnMatchesACaseInsensitivePattern() async {
+    final data =
+        await supabase.from("countries").select().ilike("name", "%aPa%");
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: columnMatchesAPattern,
+        onPressed: columnMatchesACaseInsensitivePattern,
       ),
     );
   }
