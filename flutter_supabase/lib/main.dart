cd5050731535987542ed390e3369f4fd63903954
miga-choi <miga.choi.1004@gmail.com>
2024-07-05 00:32:38 +0900

    flutter: try "overlaps" with a common element

--- flutter_supabase/lib/main.dart
+++ flutter_supabase/lib/main.dart
@@ -184,11 +184,19 @@ class _MyHomePageState extends State<MyHomePage> {
     print(data);
   }
 
+  void withACommonElement() async {
+    final data = await supabase
+        .from("issues")
+        .select("title")
+        .overlaps("tags", ["is:closed", "severity:high"]);
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: mutuallyExclusiveToARange,
+        onPressed: withACommonElement,
       ),
     );
   }
