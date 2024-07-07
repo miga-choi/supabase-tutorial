a9b736be87ff4f653d0f650ed829d4734a42905c
miga-choi <miga.choi.1004@gmail.com>
2024-07-07 21:53:42 +0900

    flutter: try "filter" match the filter

--- flutter_supabase/lib/main.dart
+++ flutter_supabase/lib/main.dart
@@ -222,11 +222,19 @@ class _MyHomePageState extends State<MyHomePage> {
     print(data);
   }
 
+  void matchTheFilter() async {
+    final data = await supabase
+        .from("countries")
+        .select()
+        .filter("name", "in", "(\"Korea\",\"Japan\")");
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: matchAtLeastOneFilter,
+        onPressed: matchTheFilter,
       ),
     );
   }
