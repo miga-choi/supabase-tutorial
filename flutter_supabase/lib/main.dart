a40fc3ff3c792f008dcd039d4c8bb1b58eedf352
miga-choi <miga.choi.1004@gmail.com>
2024-08-04 00:06:45 +0900

    flutter: try "storage.from("bucket").upload" upload a file

--- flutter_supabase/lib/main.dart
+++ flutter_supabase/lib/main.dart
@@ -1,4 +1,5 @@
 import "dart:async";
+import "dart:io";
 
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
@@ -744,11 +745,21 @@ class _MyHomePageState extends State<MyHomePage> {
     print(res);
   }
 
+  void uploadAFile() async {
+    final avatarFile = File("path/to/file");
+    final String fullPath = await supabase.storage.from("avatars").upload(
+          "public/avatar1.png",
+          avatarFile,
+          fileOptions: const FileOptions(cacheControl: "3600", upsert: false),
+        );
+    print("fullPath: $fullPath");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: emptyABucket,
+        onPressed: uploadAFile,
       ),
     );
   }
